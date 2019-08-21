2b4aee8e0175472d7560e63e3dfb741e9dd7be22
baku1413 <33036538+baku1413@users.noreply.github.com>
2019-08-21 16:28:47 +0200

    Sketch generating triangle wave.
    Code is compiling but not tested yet.

--- examples/test.ino
+++ examples/test.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DAC8571.h>
+#include <Wire.h>
 
+DAC8571 dac;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Henlo!");
+  //address: A0=0 0x98
+  //address: A0=1 0x9C
+  dac.begin(0x98);
+  Serial.println("Generating a 16-bit triangle wave");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+int reg;
+    // Run through the full 16-bit scale for a triangle wave
+    for (reg = 0; reg < 65535; reg++)
+    {
+      dac.setVoltage(reg, true);
+    }
+    for (reg = 65535; reg > 0; reg--)
+    {
+      dac.setVoltage(reg, true);
+    }  
 
 }
